3d038040a142806ae895c3171d1ed913955dabc5
Brittany <brittanyneal22@gmail.com>
2018-03-19 23:04:15 -0700

    working mode func

--- whiteboard_fu.py
+++ whiteboard_fu.py
@@ -2,6 +2,9 @@
 """
     >>> rev("The quick brown fox")
     fox brown quick The
+
+    >>> find_mode([1, 2, 2, 3, 3, 4])
+    [2, 3]
 """
 
 def rev(s):
@@ -21,6 +24,27 @@ def rev(s):
     for word in words:
         print word, 
 
+
+# find mode of list of numbers
+
+def find_mode(lst):
+    """takes in list of ints, returns mode"""
+
+    num_counts = {}
+
+    for num in lst:
+        num_counts[num] = num_counts.get(num, 0) + 1
+
+    v = sorted(num_counts.values())
+    v.reverse()
+    mode_count = v[0]
+
+    mode = []
+
+    for num in num_counts:
+        if num_counts[num] == mode_count:
+            mode.append(num)
+    return mode
 
 if __name__ == "__main__":
     import doctest
