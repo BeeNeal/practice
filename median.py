ba9bbf178b2033083b51600dab34a378cc27514f
Brittany <brittanyneal22@gmail.com>
2018-04-11 22:57:47 -0700

    working on optimizing to O(logn)

--- median.py
+++ median.py
@@ -11,18 +11,65 @@ d = []
 
 # // complete function `findMedianTwo`, which takes two sorted lists and finds the median value across both those lists
 
+# Easy O(n) solution
 def findMedianTwo(listA, listB):
     
     lst = listA + listB
     lst.sort()
-    if len(listA) % 2 == 0:
+    if len(lst) % 2 == 0:
         median = (lst[len(lst)/2] + lst[len(lst)/2 - 1]) / 2.0
     else:
         median = lst[len(lst)/2]
 
     return median
 
+
 print findMedianTwo(a, b) == 6
 print findMedianTwo(b, c) == 5.5
 print findMedianTwo(a, d) == 3
 print findMedianTwo(b, c)
+
+
+def findMedianTwo_optimized(listA, listB):
+    """Takes 2 sorted lists, finds median value in O(log n) runtime"""
+
+
+    if len(sorted_list) % 2 == 0:
+        median = (lst[len(sorted_list)/2] + sorted_list[len(sorted_list)/2 - 1]) / 2.0
+    else:
+        median = sorted_list[len(sorted_list)/2]
+
+    return median
+
+
+
+print findMedianTwo_optimized(a, b) == 6
+print findMedianTwo_optimized(b, c) == 5.5
+print findMedianTwo_optimized(a, d) == 3
+print findMedianTwo_optimized(b, c)
+
+
+# working on counting the index, so once arrive at median int, can quit, O(logn)
+def merge_two_sorted_lists(listA, listB):
+    """Implement merge portion of merge sort"""
+
+    i = 0
+    j = 0
+    count = 0
+
+    while i < len(listA) and j < len(listB):
+            if listA[i] < listB[j]:
+                i = i + 1
+            else:
+                j = j + 1
+            count += 1
+
+    while i < len(listA):
+        i = i + 1
+        count += 1
+
+    while j < len(listB):
+        j = j + 1
+        count += 1
+
+
