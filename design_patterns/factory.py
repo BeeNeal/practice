ef90e8ecd3d91d7a37a67dc81d84071defc1958f
Brittany <brittanyneal22@gmail.com>
2018-05-11 12:02:08 -0700

    factory DP

---
+++ design_patterns/factory.py
@@ -0,0 +1,37 @@
+class Dog(object):
+    """ Dog class"""
+
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        return "woof!"
+
+
+class Cat(object):
+    """ Cat class"""
+
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        return "meow!"
+
+
+def get_pet(pet="dog"):
+    """The factory method."""
+
+    pets = dict(dog=Dog("Barxter"), cat=Cat('Meowza'))
+
+    return pets[pet]
+
+d = get_pet('dog')
+c = get_pet('cat')
+
+print d.speak()
+print c.speak()
+
+
+# Encapulates object creation
+# Useful when have uncertainties in types of objects
+
