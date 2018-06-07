d4dd46d136dba5927594d58c69594f0218225afe
Brittany <brittanyneal22@gmail.com>
2018-06-07 14:25:55 -0700

    added vertex class - about to update add_edge method

--- google_prep/graphs.py
+++ google_prep/graphs.py
@@ -18,16 +18,47 @@
 
 # graph = {'v0':['v1', 'v2'], 'v1', 'v2', 'v3', 'v4'}
 
+class Graph(object):
+    """graph object"""
+
+    def __init__(self, vertices):
+        if vertices:
+            self.vertices = vertices
+        else:
+            self.vertices = []
+
+    def add_vertex(self, vert):
+        """adds an instance of Vertex to the graph"""
+
+        self.vertices.append(vert)
+
+    def add_edge(self, from_vert, to_vert, weight):
+        """adds an edge to a vertex, and includes the weight"""
+
+        # add to adj key in dict at the vert variable's 
+        # need to update the var without overwriting
+        # instead of variables, they should probably be objects
+        # OR - how to update 
+
+        # if from_vert in self.vertices:
+        #     self.vertices['{vert}'.format(vert=from_vert)]
+        # self.vertices.append({'id':})
+
+    def get_vertices(self):
+        """returns all vertices in graph"""
+
+        return self.vertices
+
 
 # adj determined by direction
-v0 = {'id': 'v0', 'adj': {'v1':5, 'v5':2}}
-v1 = {'id': 'v1', 'adj': {'v2':4}}
-v2 = {'id': 'v2', 'adj': {'v3':9}}
-v3 = {'id': 'v3', 'adj': {'v4':7, 'v5':3}}
-v4 = {'id': 'v4', 'adj': {'v0':1}}
-v5 = {'id': 'v5', 'adj': {'v2':1, 'v4':8}}
+v0 = {'id': 'v0', 'adj': {'v1': 5, 'v5': 2}}
+v1 = {'id': 'v1', 'adj': {'v2': 4}}
+v2 = {'id': 'v2', 'adj': {'v3': 9}}
+v3 = {'id': 'v3', 'adj': {'v4': 7, 'v5': 3}}
+v4 = {'id': 'v4', 'adj': {'v0': 1}}
+v5 = {'id': 'v5', 'adj': {'v2': 1, 'v4': 8}}
 
-graph = [v0, v1, v2, v3, v4]
+my_graph = Graph([v0, v1, v2, v3, v4])
 
 # v0 = {'id': 'v0', 'adj': {'v1':5, 'v5':2}}
 # v1 = {'id': 'v1', 'adj': {'v2':4}}
@@ -36,5 +67,8 @@ graph = [v0, v1, v2, v3, v4]
 # v4 = {'id': 'v4', 'adj': {'v0':1, 'v3':7, 'v5':8}}
 # v5 = {'id': 'v5', 'adj': {'v0':1, 'v2': , 'v3':3, 'v4':8}}
 
-for v in graph:
-    print v['id'], v['adj']
+# for v in my_graph.vertices:
+#     print v['id'], v['adj']
+
+print my_graph.get_vertices
+print my_graph.vertices.values()
