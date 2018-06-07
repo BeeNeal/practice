20b9ceb83a4072e5a122ac034a7900349077efa5
Brittany <brittanyneal22@gmail.com>
2018-06-06 23:22:01 -0700

    implementing graph

--- google_prep/graphs.py
+++ google_prep/graphs.py
@@ -13,3 +13,18 @@
     # but not a good way to store sparse data
 
 # Good way to store sparse data is with an adjacency list
+    # master list of all vertices
+    # all vertices these vertices are adjacent to are stored in dictionary
+
+# graph = {'v0':['v1', 'v2'], 'v1', 'v2', 'v3', 'v4'}
+
+
+# graph = {}
+graph = [v0, v1, v2, v3, v4]
+
+v0 = {'id': 'v0', 'adj': {'v1':5, 'v5':2}}
+v1 = {'id': 'v1', 'adj': {'v2':4}}
+v2 = {'id': 'v2', 'adj': {'v1':4, 'v3':9, 'v5':1}}
+v3 = {'id': 'v3', 'adj': {'v2':9, 'v4':7, 'v5':3}}
+v4 = {'id': 'v4', 'adj': {'v0':1, 'v3':7, 'v5':8}}
+v5 = {'id': 'v5', 'adj': {'v0':1, 'v2': , 'v3':3, 'v4':8}}
