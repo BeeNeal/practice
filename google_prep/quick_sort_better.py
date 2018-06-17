33671f3838ec439a0ae4100399e20ddc621b8331
Brittany <brittanyneal22@gmail.com>
2018-06-17 14:03:50 -0700

    implementing map from arrays

--- google_prep/quick_sort_better.py
+++ google_prep/quick_sort_better.py
@@ -7,7 +7,12 @@ def quick_sort(A):
 def quick_sort2(A, low, high):
     """Recursively calls QS on both sides of pivot"""
 
-    if low < high:  # if more than one num in list, do the QS
+    # Uses selection sort if # of items in lst less than 20
+    if len(A) < 20 and low < high:
+        print "using selection sort instead"
+        quick_selection(A, low, high)
+
+    elif low < high:  # if more than one num in list, do the QS
         p = partition(A, low, high)
         quick_sort2(A, low, p - 1)
         quick_sort2(A, p + 1, high)
@@ -17,7 +22,7 @@ def partition(A, low, high):
     """Performs the quick sort"""
 
     # get pivot index and pivot value
-    pivot_index = get_pivot(A)
+    pivot_index = get_pivot(A, low, high)
     pivot_value = A[pivot_index]
 
     # swap the pivot number and low position, border becomes low position
@@ -38,7 +43,7 @@ def get_pivot(A, low, high):
 
     mid = low + high / 2
 
-    s = sorted(A[low, A[mid], A[high]])
+    s = sorted([A[low], A[mid], A[high]])
     if s[1] == A[low]:
         return low
     elif s[1] == A[mid]:
@@ -66,3 +71,20 @@ def get_pivot(A, low, high):
 #     else:
 #         pivot_i = c
 
+
+def quick_selection(x, first, last):
+    """O(n^2), useful for datasets less than 10,000"""
+
+    # set our first loop, setting the min index as first item's index 
+    # (b/c that's all we've seen so far)
+    for i in range(first, last):
+        minIndex = i
+        # set second loop, go through list to find smallest num
+        for j in range(i + 1, last + 1):
+            if x[j] < x[minIndex]:
+                minIndex = j
+        # After the second loop, as long as we aren't swapping same thing in place,
+        # swap index of smallest num with index of i (i will be the first item in 
+        # the nonsorted part of the list)
+        if minIndex != i:
+            x[i], x[minIndex] = x[minIndex], x[i]
