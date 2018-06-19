3ad8b4cb8352fb087d9d5a6caef44da29a460c42
Brittany <brittanyneal22@gmail.com>
2018-06-18 22:17:02 -0700

    added put functionality, laying foundation for upsizing lists when full

---
+++ google_prep/implement_dictionary.py
@@ -0,0 +1,34 @@
+# This all works if the key is an integer, but what about other types of immutables?
+#  Use ord? but that would be O(n) for the key
+
+class HashTable(object):
+    """Dictionary object"""
+
+    def __init__(self):
+        self.size = 11  # size is prime for most efficient collision resolution
+        self.slots = [None] * self.size
+        self.data = [None] * self.size
+
+    def put(self, key, data):
+
+        hash_value = self.hash_function(key, len(self.slots))
+        if self.slots[hash_value] == None:
+            self.slots[hash_value] = key
+            self.data[hash_value] = data
+        else:
+            if self.slots[hash_value] == key:
+                self.data[hash_value] = data  # overwrite key if already there
+            else:
+                next_slot = self.rehash(hash_value, len(self.slots))
+                times_reslot = 0
+                while self.next_slot is not None and self.next_slot != key:
+                    next_slot = self.rehash(hash_value, len(self.slots))
+                    times_reslot += 1
+
+    def hash_function(self, key, size):
+        """In this case we assume our keys will be integers"""
+
+        return key % size
+
+    def rehash(self, oldhash, size):
+        return (oldhash + 1) % size
