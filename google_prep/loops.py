1faeafda273a3568cb870afaff19963a9c55510f
Brittany <brittanyneal22@gmail.com>
2018-06-21 22:59:43 -0700

    sum_nums with regex

--- google_prep/loops.py
+++ google_prep/loops.py
@@ -10,3 +10,41 @@ def has22(nums):
             else:
                 continue
     return False
+
+
+def sum_nums(s):
+
+    total = 0
+
+    for i in xrange(len(s)):
+        amt_nums = 0
+        if s[i].isdigit:
+            current_pos = i
+            amt_nums += 1
+        elif not s[i].isdigit and current_pos:
+            total += int(s[current_pos - amt_nums:current_pos])
+            current_pos = None
+            amt_nums = 0
+        else:
+            amt_nums = 0
+    return total
+
+    # 'this is a string'
+    # start = 0
+    # for i in s:
+    #     if i.isdigit():
+    #         start = i
+    #     if i == ' ':
+    #         chunk = 
+
+import re
+
+def sum_nums(s):
+
+    total = 0
+    nums = filter(None, re.split(r'(\d+)', s))
+    for item in nums:
+        if item.isdigit():
+            total += int(item)
+
+    return total
